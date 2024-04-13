3194d2568ce6898956eda0be128f88fb53889eb1
Dino Source <dinosource.cpp@gmail.com>
2024-04-12 22:23:03 -0700

    Add mappings and options

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("x", "jk", "<ESC>")
+map("v", "jk", "<ESC>")
+map("o", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,12 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+
+vim.opt.mouse = "a" -- allow the mouse to be used in neovim
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 4 -- insert 2 spaces for a tab
+vim.opt.cursorline = true -- highlight the current line
+vim.opt.number = true -- set numbered lines
+vim.opt.relativenumber = true -- set relative numbered lines
