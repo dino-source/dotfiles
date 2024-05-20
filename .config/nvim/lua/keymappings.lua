5305439bb44341347e61696322311c076d87af2b
Dino Source <dinosource.cpp@gmail.com>
2024-05-20 10:27:43 -0700

    Add more keybindings + fix typos

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -2,10 +2,16 @@
 local options = { noremap = true }
 INSERT_MODE = 'i'
 NORMAL_MODE = 'n'
-vim.keymap.set(INSERT_MODE, 'jk', '<Esc>', options)                 -- Use 'jk' as <Escape> key
-vim.keymap.set(NORMAL_MODE, '<leader>t', ':ter<CR><S-a>', options)  -- open terminal
+vim.keymap.set(INSERT_MODE, 'jk', '<Esc>', options)                  -- Use 'jk' as <Escape> key
+vim.keymap.set(NORMAL_MODE, '<leader>t', ':ter<CR><S-a>', options)   -- open terminal
 vim.keymap.set(NORMAL_MODE, '<leader>w', ':q!<CR>', options)         -- quit current buffer
-vim.keymap.set(NORMAL_MODE, '<leader>q', ':wqa<CR>', options)       -- write changes, quit all buffers
+vim.keymap.set(NORMAL_MODE, '<leader>q', ':wqa<CR>', options)        -- write changes, quit all buffers
 vim.keymap.set(NORMAL_MODE, '<leader>n', ':bn<CR>', options)         -- go to next buffer
-vim.keymap.set(NORMAL_MODE, '<leader>n', ':bp<CR>', options)         -- go to prev buffer
+vim.keymap.set(NORMAL_MODE, '<leader>p', ':bp<CR>', options)         -- go to prev buffer
+
+-- Alternative keybindigs (in case leader key just stops working):
+vim.keymap.set(NORMAL_MODE, '<A-w>', ':q!<CR>', options)             -- quit current buffer
+vim.keymap.set(NORMAL_MODE, '<A-e>', ':Neotree filesystem reveal left<CR>', options) -- open neo-tree
+vim.keymap.set(NORMAL_MODE, '<A-n>', ':bn<CR>', options)             -- go to next buffer
+vim.keymap.set(NORMAL_MODE, '<A-p>', ':bp<CR>', options)             -- go to prev buffer
 
