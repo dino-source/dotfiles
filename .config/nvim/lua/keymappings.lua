a5a966f393079d005b1edd92135069af827e7f92
Dino Source <dinosource.cpp@gmail.com>
2024-05-20 10:17:10 -0700

    Add more keymappings

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -4,6 +4,8 @@ INSERT_MODE = 'i'
 NORMAL_MODE = 'n'
 vim.keymap.set(INSERT_MODE, 'jk', '<Esc>', options)                 -- Use 'jk' as <Escape> key
 vim.keymap.set(NORMAL_MODE, '<leader>t', ':ter<CR><S-a>', options)  -- open terminal
-vim.keymap.set(NORMAL_MODE, '<leader>w', ':q<CR>', options)         -- quit current buffer
+vim.keymap.set(NORMAL_MODE, '<leader>w', ':n!<CR>', options)         -- quit current buffer
 vim.keymap.set(NORMAL_MODE, '<leader>q', ':wqa<CR>', options)       -- write changes, quit all buffers
+vim.keymap.set(NORMAL_MODE, '<leader>n', ':bn<CR>', options)         -- go to next buffer
+vim.keymap.set(NORMAL_MODE, '<leader>n', ':bp<CR>', options)         -- go to prev buffer
 
