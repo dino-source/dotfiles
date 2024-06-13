6b5890802423161c06d055555bd29cd6d8d8e848
Dino Source <dinosource.cpp@gmail.com>
2024-06-13 14:56:42 -0700

    Move none-ls keybinding to keymappings.lua

--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -12,6 +12,7 @@ local alt_q = '<A-q>'
 local alt_e = '<A-e>'
 local alt_n = '<A-n>'
 local alt_p = '<A-p>'
+local alt_f = '<A-f>'
 
 local open_terminal = ':ter<CR><S-a>'
 vim.keymap.set(mode.normal, alt_t, open_terminal, options)
@@ -36,3 +37,6 @@ vim.keymap.set(mode.normal, alt_k, move_current_line_up,options)
 
 local move_current_line_down = 'ddp'
 vim.keymap.set(mode.normal, alt_j, move_current_line_down,options)
+
+-- Enable auto-formatting using none-ls plugin
+vim.keymap.set(mode.normal, alt_f, vim.lsp.buf.format, options)
