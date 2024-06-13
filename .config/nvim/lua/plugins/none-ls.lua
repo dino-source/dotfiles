d8b133f7a1f457c332e52564ba7d5d05f89ba933
Dino Source <dinosource.cpp@gmail.com>
2024-06-13 14:46:27 -0700

    Fix none-ls

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,11 +6,8 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.completion.spell,
                 null_ls.builtins.diagnostics.cmake_lint,
-                null_ls.builtins.formatting.cmake_format, -- Parse cmake listfiles and format them nicely
-                null_ls.builtins.diagnostics.cppcheck, -- A tool for fast static analysis of C/C++ code
-                null_ls.builtins.formatting.clang_format, -- A tool to format C/C++ code
+                null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.black, -- The uncompromising Python code formatter
             },
         })
